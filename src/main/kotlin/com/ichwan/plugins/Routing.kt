dc634cca8b038dcc183a6663087bb9a728261dfa
ichwansh03 <ichwansholihin03@gmail.com>
2023-09-16 14:29:24 +0700

    fixed: change route and update static resources

--- src/main/kotlin/com/ichwan/plugins/Routing.kt
+++ src/main/kotlin/com/ichwan/plugins/Routing.kt
@@ -1,5 +1,6 @@
 package com.ichwan.plugins
 
+import com.ichwan.routes.randomRabbit
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
 import io.ktor.server.response.*
@@ -7,12 +8,8 @@ import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
+        randomRabbit()
         // Static plugin. Try to access `/static/index.html`
-        static("/static") {
-            resources("static")
-        }
+        staticResources("/", "static")
     }
 }
