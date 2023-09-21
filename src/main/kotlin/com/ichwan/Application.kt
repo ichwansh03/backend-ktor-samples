3f684516c358e617a69c69ca5095540e48785041
ichwansh03 <ichwansholihin03@gmail.com>
2023-09-21 13:57:27 +0700

    starter project crud with ktor

---
+++ src/main/kotlin/com/ichwan/Application.kt
@@ -0,0 +1,14 @@
+package com.ichwan
+
+import com.ichwan.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureMonitoring()
+    configureRouting()
+}
