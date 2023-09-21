e923b957b951eeb77e6dd1c1a9c4e7bbdb1486f8
ichwansh03 <ichwansholihin03@gmail.com>
2023-09-22 03:55:28 +0700

    fixed: add `@Serkalizable` and change price to `Int`

--- src/main/kotlin/com/ichwan/entity/Product.kt
+++ src/main/kotlin/com/ichwan/entity/Product.kt
@@ -1,10 +1,13 @@
 package com.ichwan.entity
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Product (
 
     var id: Int,
     var name: String,
     var category: String,
-    var price: Long,
+    var price: Int,
     var stock: Int
 )
