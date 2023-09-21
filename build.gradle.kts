7df8999b13d9703c8cbf3bdc7e18d908cfba400e
ichwansh03 <ichwansholihin03@gmail.com>
2023-09-22 03:54:09 +0700

    chore: add ktorm and mysql connector library

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,9 @@ dependencies {
     implementation("io.ktor:ktor-server-call-logging-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("mysql:mysql-connector-java:8.0.28")
+    implementation("org.ktorm:ktorm-core:3.2.0")
+    implementation("org.ktorm:ktorm-support-mysql:3.2.0")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
