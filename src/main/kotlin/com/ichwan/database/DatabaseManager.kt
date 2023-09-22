8a2c02b3be1c048a0cd6dd127ca1385de7c5e959
ichwansh03 <ichwansholihin03@gmail.com>
2023-09-22 10:37:42 +0700

    chore: add product service from database

--- src/main/kotlin/com/ichwan/database/DatabaseManager.kt
+++ src/main/kotlin/com/ichwan/database/DatabaseManager.kt
@@ -1,6 +1,12 @@
 package com.ichwan.database
 
+import com.ichwan.entity.Product
 import org.ktorm.database.Database
+import org.ktorm.dsl.delete
+import org.ktorm.dsl.eq
+import org.ktorm.dsl.insertAndGenerateKey
+import org.ktorm.dsl.update
+import org.ktorm.entity.firstOrNull
 import org.ktorm.entity.sequenceOf
 import org.ktorm.entity.toList
 
@@ -20,5 +26,40 @@ class DatabaseManager {
 
     fun getAllProducts(): List<DBProductEntity> {
         return ktormDb.sequenceOf(TableEntities).toList()
+    }
+
+    fun getProduct(id: Int) : DBProductEntity? {
+        return ktormDb.sequenceOf(TableEntities).firstOrNull { it.id.eq(id) }
+    }
+
+    fun addProduct(product: Product) : Product {
+        var productId = ktormDb.insertAndGenerateKey(TableEntities) {
+            set(TableEntities.name, product.name)
+            set(TableEntities.category, product.category)
+            set(TableEntities.price, product.price)
+            set(TableEntities.stock, product.stock)
+        } as Int
+
+        return Product(productId, product.name, product.category, product.price, product.stock)
+    }
+
+    fun updateProduct(id: Int, product: Product): Boolean {
+        val update = ktormDb.update(TableEntities) {
+            set(TableEntities.name, product.name)
+            set(TableEntities.category, product.category)
+            set(TableEntities.price, product.price)
+            set(TableEntities.stock, product.stock)
+            where { it.id.eq(id) }
+        }
+
+        return update > 0
+    }
+
+    fun deleteProduct(id: Int) : Boolean{
+        val delete = ktormDb.delete(TableEntities) {
+            it.id.eq(id)
+        }
+
+        return delete > 0
     }
 }
